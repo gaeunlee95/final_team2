0a1d57ea3c8ee6a1fd3aceab41315ff57631bdf8
gaeunlee95 <yuebn22@naver.com>
2024-01-29 23:13:31 +0900

    first commit

---
+++ final_team2/src/main/java/com/khit/recruit/FinalTeam2Application.java
@@ -0,0 +1,13 @@
+package com.khit.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTeam2Application.class, args);
+	}
+
+}
